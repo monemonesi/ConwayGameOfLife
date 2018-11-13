fe5a6af044d8905bb9c918e7d7f7dc3ccf6d3e58
Roberto Monesi <roberto.monesi@outlook.com>
2018-11-13 06:15:32 +0000

    Create empty shell/ namespaces

--- ConwayGameOfLife/ConwayGameOfLife/View/GameOfLifeView.xaml.cs
+++ ConwayGameOfLife/ConwayGameOfLife/View/GameOfLifeView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ConwayGameOfLife
+namespace ConwayGameOfLife.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for GameOfLifeView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class GameOfLifeView : Window
     {
-        public MainWindow()
+        public GameOfLifeView()
         {
             InitializeComponent();
         }
